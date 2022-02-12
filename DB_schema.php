cc16e6637f93949aac7040a533d7728d11c9e61a
Ben Harvey <letsgocamping88@aol.com>
2022-02-12 20:55:35 +0000

    Add files via upload

---
+++ DB_schema.php
@@ -0,0 +1,20 @@
+<?php
+/*
+login as root user by sudo MariaDB or sudo mysql
+
+
+CREATE DATABASE 'nommies';
+USE nommies;
+
+CREATE TABLE gameTable (
+    `gameid` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+    `gameObject` text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `scoreObject` text COLLATE utf8mb4_unicode_ci NOT NULL,
+    `gameStarted` tinyint(1) NOT NULL DEFAULT 0,
+    `gameHash` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+    PRIMARY KEY (`gameid`)
+);
+
+*/
+
+?>
